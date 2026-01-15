6517bd65855ca8c5db760ba7c9b4a73ae5ded631
RodrigoMZC <rodrigomazuca3@gmail.com>
2026-01-14 18:35:55 -0600

    Inicio del backend: previo a implementacion de procesamiento

---
+++ backend-newslatter/database/migrations/2026_01_14_221629_create_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('title')->notNullable();
+            $table->string('category')->notNullable()->index();
+            $table->text('description')->notNullable();
+            $table->longText('content')->notNullable();
+            $table->string('url')->unique(); // url a la noticia original
+            $table->string('image');
+            $table->dateTimeTz('published_at')->index();
+            $table->decimal('score', 2, 1)->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
